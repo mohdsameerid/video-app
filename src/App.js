864dfbd1b4f382f5fb66073b1dec48f557017baf
mohdsameerid <mohdsameeredu@gmail.com>
2023-03-06 09:08:27 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <span>Humburger</span>
+        <span>Wetube LOGO</span>
+      </div>
+      <span>Search Bar</span>
+      <span>User icon</span>
+    </div>
+  );
+}
+
+export default App;
