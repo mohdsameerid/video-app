d3d11f3f1321d3904da73084f8bcbe166b79f692
mohdsameerid <mohdsameeredu@gmail.com>
2023-03-24 02:17:37 -0700

    youtube key auth change

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const YOUTUBE_API_KEY = "AIzaSyCSb_KhNWy_ZIZd2b_C6kHPsw1zKaWyiMM";
+const YOUTUBE_API_KEY = "AIzaSyBrFZBavzJPg_wppLje-V2T_MwFlyH9EVI";
 
 export const GOOGLE_YOUTUBE_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
